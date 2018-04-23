b7a5849856eb312c0de4610b9c99863660b83ffc
chouheiwa <849131492@qq.com>
2018-04-23 16:26:14 +0800

    First commit for inital

---
+++ localConfig.py
@@ -0,0 +1,111 @@
+# encoding: utf-8
+import json
+
+class LocalFile:
+    """
+    file_name 文件名
+    """
+    dump = None  # type: str
+
+    def __init__(self, file_name, is_json=False):
+        self.file_name = file_name
+        self.dump = None
+        self.total_string = None
+        self.is_json = is_json
+
+    def load_from_file(self):
+        try:
+            file = open(self.file_name)
+            file_text = file.read()
+            if self.is_json:
+                self.dump = json.loads(file_text)
+            else:
+                self.dump = file_text
+            file.close()
+        except Exception, e:
+            self.dump = None
+
+    def write_to_file(self):
+        if self.is_json:
+            if not isinstance(self.total_string, str):
+                self.total_string = json.dumps(self.total_string, indent=4)
+        with open(self.file_name, 'w') as f:
+            f.write(self.total_string)
+
+
+class BaseConfig:
+    configuration = None  # type: str
+    build_targets = None  # type: list
+    workspace_path = None  # type: str
+    schemes = None  # type: list
+    local_text = None  # type: str
+    sharedBaseConfig = None # type: BaseConfig
+
+    @classmethod
+    def shared_base_config(cls):
+        # type: () -> BaseConfig
+        if cls.sharedBaseConfig is None:
+            cls.sharedBaseConfig = cls()
+        return cls.sharedBaseConfig;
+
+    def __init__(self):
+        self.workspace_name = None
+        self.local_text = None
+        self.workspace_path = None
+        self.schemes = None
+        self.build_targets = None
+        self.configuration = None
+        self.final_archive_path = None
+        local = LocalFile('setting.ini')
+        local.load_from_file()
+        if local.dump is None:
+            print "setting.ini doesn't exist.Creat an default one.You need to restart again"
+            self.creat_config()
+            exit(2)
+        self.load_local_bin()
+        print self.build_targets
+
+    def load_local_bin(self):
+        # type: () -> void
+        local = LocalFile('setting.ini')
+        local.load_from_file()
+        ignore_prefix_array = ['#']
+        array = local.dump.split('\n')
+        for sentense in array:
+            if len(sentense) == 0:
+                continue
+            if sentense[0] in ignore_prefix_array:
+                continue
+            result_array = sentense.split('=')
+            if len(result_array) <= 1:
+                continue
+            key = result_array[0].replace(' ', '')
+            value = result_array[-1].replace(' ', '')
+            try:
+                setattr(self, key, json.loads(value))
+            except Exception,e:
+                setattr(self, key, value)
+
+    def save_to_file(self):
+        local = LocalFile('setting.ini', False)
+        local.total_string = self.local_text
+        local.write_to_file()
+
+    def creat_config(self):
+        self.local_text = """#构建目录所在绝对路径 例如 workspace_path = /example/
+workspace_path =
+#构建项目工程名所在绝对路径 例如 workspace_name = Example.xcodeproj
+workspace_name =
+#需要构建的全部scheme 例如 schemes = ['example']
+schemes =
+#需要构建的全部型号
+#包括iOS|iOSSimulator|macOS|tvOS|tvOSSimulator|watchOS|watchOSSimulator
+#例如 build_targets = ['iOS','iOSSimulator']
+build_targets =
+#请输入最终完成构建的类型
+#包括Release|Debug 以及其他自定构建类型
+#例如 configuration = Release
+configuration = 
+#最终生成的包的绝对路径 
+final_archive_path = """
+        self.save_to_file()
---
+++ run_command.py
@@ -0,0 +1,125 @@
+from localConfig import BaseConfig
+import os, re, random, string,getopt,sys
+
+
+# -sdk .+$
+class re_python:
+    @staticmethod
+    def re_find(serch_str, in_str):
+        # type: (str, str) -> str
+        key = in_str
+        p1 = serch_str
+        pattern1 = re.compile(p1)
+        matcher1 = re.search(pattern1, key)
+        if matcher1 is None:
+            return matcher1
+        return matcher1.group(0)
+
+
+class base_commnd_class:
+    def __init__(self):
+        self.work_space_path = BaseConfig.shared_base_config().workspace_path
+        self.work_space_name = BaseConfig.shared_base_config().workspace_name
+        self.build_schemes = BaseConfig.shared_base_config().schemes
+        self.total_need_builds = BaseConfig.shared_base_config().build_targets
+        self.build_configuration = BaseConfig.shared_base_config().configuration
+        self.final_archive_path = BaseConfig.shared_base_config().final_archive_path
+
+
+class run_os_command:
+    def __init__(self):
+        pass
+
+    def run_script_get_result(self, script):
+        # type: (str) -> str
+        """
+
+        :type script: str
+        """
+        result = os.popen(script)
+        return result.read()
+
+    def generate_shell(self, string):
+        print string
+        file_name = self.generate_random() + '.sh'
+        with open(file_name, 'w+') as f:
+            f.write(string)
+        os.system("chmod +x {}".format(file_name))
+        print "run shell script"
+        os.system("./{}".format(file_name))
+        os.system("rm -f {}".format(file_name))
+
+    def generate_random(self):
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(20))
+
+
+class xbuild_commnd(run_os_command):
+    xcode_build_sdk = None  # type: Dict[str, str]
+
+    def __init__(self, base_command):
+        self.xcode_build_sdk = {}
+        self.get_all_can_build_sdk()
+        self.base_command = base_command
+        pass
+
+    def build_all(self):
+        for result in self.base_command.total_need_builds:
+            self.xcode_build(self.base_command.work_space_path + self.base_command.work_space_name,
+                             self.base_command.build_schemes, self.base_command.build_configuration,
+                             self.xcode_build_sdk.get(result))
+
+    def get_all_can_build_sdk(self):
+        shell_string = self.run_script_get_result('xcodebuild -showsdks')
+        result_arr = shell_string.split('\n')
+        while result_arr.__contains__(''):
+            result_arr.remove('')
+        for i in range(0, len(result_arr)):
+            result_str = result_arr[i]
+            if i % 2 == 1:
+                self.xcode_build_sdk[result_arr[i - 1].replace(' SDKs:', '').replace(' ', '')] = re_python.re_find(
+                    r'-sdk .+$', result_str)
+
+    def xcode_build(self, project_path, schemes, configuration, sdk):
+        # type: (string, list, string, string) -> None
+        build_type = ''
+        if project_path.endswith('xcodeproj'):
+            build_type = '-project'
+        elif project_path.endswith('xcworkspace'):
+            build_type = '-workspace'
+        final_str = "xcodebuild {} {} -scheme {} -configuration {} {}".format(build_type, project_path,
+                                                                              ' '.join(schemes), configuration, sdk)
+        self.generate_shell(final_str)
+
+    def find_a_file(self):
+        file_lists = os.listdir(self.base_command.work_space_path)
+        for result_str in file_lists:
+            if self.base_command.build_configuration in result_str:
+                extra_command = 'macOS'
+                if '-' in result_str:
+                    extra_command = result_str.split('-')[-1]
+                for final_a_file_name in os.listdir(self.base_command.work_space_path + result_str):
+                    if '.a' in final_a_file_name:
+                        lastest_name = final_a_file_name.replace('.a', '_' + extra_command) + '.a'
+                        shell_script = "mv {} {}\n".format(self.base_command.work_space_path + result_str + '/' + final_a_file_name,self.base_command.final_archive_path + lastest_name)
+                        shell_script = shell_script + "rm -rf {}".format(self.base_command.work_space_path + result_str)
+                        self.generate_shell(shell_script)
+
+    def zip_final(self,final_name):
+        shell_script = "cd {}\n".format(self.base_command.final_archive_path)
+        shell_script = shell_script + "zip -m {}.zip *.a".format(final_name)
+        self.generate_shell(shell_script)
+
+
+def main(argv):
+    try:
+        options, args = getopt.getopt(argv, "hp:b:", ["help", "password=", "buildNum=","jenkinsWorkSpacePath=","jenkinsProjectName=","optionsPlist=","svn=","borderlessCase="])
+    except getopt.GetoptError:
+        print "error input"
+        sys.exit(2)
+
+if __name__ == '__main__':
+    BaseConfig.shared_base_config()
+    xbuild = xbuild_commnd(base_commnd_class())
+    xbuild.find_a_file()
+    xbuild.zip_final()
+
